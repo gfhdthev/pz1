7bdcd3b51e96183ce69bb4115a3542af15383f37
gfhdthev <akkglow@tutamail.com>
2025-02-25 13:22:10 +0300

    third

--- class.py
+++ class.py
@@ -27,10 +27,18 @@ class Birds:
         else:
             print(f'{self.name} не может летать')
 
+    def flying_range(self):
+        if self.flying_ability is True:
+            dist = int(long)*float(self.size)
+            print(f'{self.name} полетела {long} за {dist}')
+        else:
+            print(f'{self.name} не может летать, а только ходит')
+
     #деструктор
     def __del__(self):
         print(f'Птица {self.name} удалена')
 
+long = 1000 #дистанция
 
 #создание объектов
 bird1 = Birds('golyb', True, 0.4)
@@ -42,6 +50,11 @@ bird1.sounding()
 
 bird1.flying()
 bird4.flying()
+
+bird1.flying_range()
+bird4.flying_range()
+
+
 
 #функция для общего сна
 def night():
