76031230bd9059b845050ab303808627d7c382e2
gfhdthev <akkglow@tutamail.com>
2025-02-25 12:45:55 +0300

    second

--- class.py
+++ class.py
@@ -21,6 +21,12 @@ class Birds:
     def sleeping(self):
         print(f'Птица {self.name} спит')
 
+    def flying(self):
+        if self.flying_ability is True:
+            print(f'{self.name} полетела')
+        else:
+            print(f'{self.name} не может летать')
+
     #деструктор
     def __del__(self):
         print(f'Птица {self.name} удалена')
@@ -28,16 +34,22 @@ class Birds:
 
 #создание объектов
 bird1 = Birds('golyb', True, 0.4)
+bird4 = Birds('pinguin', False, 0.7)
 
 #использование методов
 bird1.eating()
 bird1.sounding()
 
+bird1.flying()
+bird4.flying()
+
 #функция для общего сна
 def night():
     bird2 = Birds('eagle', True, 1.5)
     bird3 = Birds('ostrich', False, 0.5)
+    bird1.sleeping()
     bird2.sleeping()
     bird3.sleeping()
+    bird4.sleeping()
 
 night()
